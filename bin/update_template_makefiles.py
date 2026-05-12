4865c22dd1dd2d18793ca23930fdfef43246af24
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-05-13 07:22:13 +0900

    WindowsにおけるUTF-8 Pythonの文字化け対策

--- bin/update_template_makefiles.py
+++ bin/update_template_makefiles.py
@@ -18,6 +18,9 @@ import argparse
 import sys
 from pathlib import Path
 
+sys.stdout.reconfigure(encoding="utf-8")
+sys.stderr.reconfigure(encoding="utf-8")
+
 
 TEMPLATE_MAP = {
     "# app 配下 makefile テンプレート": Path(
